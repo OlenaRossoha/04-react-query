351fd2b909bc994a3a0e5c56eb676c0ea1bc66e7
Olena <qa.job.rav@gmail.com>
2025-07-11 07:12:00 +0300

    commit message

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -1,22 +1,3 @@
-/*export interface Movie {
-  id: number;
-  poster_path: string;
-  backdrop_path: string;
-  title: string;
-  overview: string;
-  release_date: string;
-  vote_average: number;
-}
-
-export interface MoviesResponse {
-  results: Movie[];
-  total_pages: number;
-  page: number;
-  total_results: number;
-}*/
-
-// src/types/movie.ts
-
 export interface Movie {
   id: number;
   poster_path: string | null;
